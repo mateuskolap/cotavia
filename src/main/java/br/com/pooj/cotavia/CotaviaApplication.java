00b736ce5ab130796cdc0594ce6af7bea60b8b49
mateuskolap <mateuskolap@gmail.com>
2025-03-20 21:39:50 -0300

    Adicionada estrutura do projeto.

---
+++ src/main/java/br/com/pooj/cotavia/CotaviaApplication.java
@@ -0,0 +1,13 @@
+package br.com.pooj.cotavia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotaviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotaviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pooj/cotavia/CotaviaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pooj.cotavia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotaviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
